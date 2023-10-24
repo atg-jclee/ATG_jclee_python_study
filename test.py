84cc19ddd0136dda100d86b52b18643a0dd2c148
atg-jclee <jclee@atg.co.kr>
2023-10-24 15:18:46 +0900

    gitignore test

--- test.py
+++ test.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[6]:
+
+
+import time
+import asyncio
+
+
+# In[7]:
+
+
+def print_current_time() :
+    print(f"Now : {time.strftime('%X')}")
+    time.sleep(1)
+
+
+# In[8]:
 
 
 def main():
-    print('World!')
+    print('print_current_time')
+    while True:
+        print_current_time()
 
 
-# In[3]:
+# In[9]:
 
 
 if __name__ == '__main__':
-    print('Hello')
+    print('main')
     main()
 
